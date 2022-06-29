652b1dcddd1df478c0fb3a8c83b808208394810a
genrosales11-minjcA <genrosales11@gmail.com>
2022-06-28 23:58:36 -0700

    final

--- quizgame.js
+++ quizgame.js
@@ -1,56 +1,137 @@
-// create a quiz class
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const progressBarFul = document.querySelector('#progressBarFull');
+// CREATE A QUIZ CLASS
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
 
-//quiz questions about javascrip add answer
-let currentQuestion = {}
-    let acceptingAnswers = true
-    let score = 0
-    let questionCounter = 0
-    let availableQuestin = []
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
 
-    let question = [
-        {
-            question:'what is 2 + 2?',
-            choice1: '2',
-            choice1: '3',
-            choice1: '32',
-            choice1: '34',
-            answer: 2,
-        },
-        {
-            question:'what js',
-            choice1: '2',
-            choice1: '3',
-            choice1: '32',
-            choice1: 'js',
-            answer: js,
-        },
-        {
-            question:'what is oso',
-            choice1: '2',
-            choice1: '3',
-            choice1: 'baby',
-            choice1: '34',
-            answer: baby,
-        },
-        {
-            question:'what is 2 + 2?',
-            choice1: '2',
-            choice1: '3',
-            choice1: '32',
-            choice1: '34',
-            answer: 2,
-        },
-        {
-            question:'what is my name?',
-            choice1: '2',
-            choice1: '3',
-            choice1: 'gen',
-            choice1: '34',
-            answer: gen,
-        },
-    ]
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+// Create a question Class
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+// NOW DISPLAY THE QUESTIONS
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        let questionElement = document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for (let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+// GUESS ANSWER
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+};
+
+// SHOW QUIZ PROGRESS
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let ProgressElement = document.getElementById("progress");
+    ProgressElement.innerHTML =
+        `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
+
+// SHOW SCORES
+function showScores() {
+    let quizEndHTML =
+        `
+    <h1>Quiz Completed</h1>
+    <h2 id='score'> Your scored: ${quiz.score} of ${quiz.questions.length}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Take Quiz Again</a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+};
+
+// create questions here
+let questions = [
+    new Question(
+    "How does a WHILE loop start?", ["while (i <= 10)", "while (i <= 10; i++)", "while i = 1 to 10", "While i+++10"], 
+        "while (i <= 10)  "
+    ),
+    new Question(
+        "Inside which HTML element do we put the JavaScript?", ["<script>", "<javascript",
+    "<scripting>", "<js>"], "<script>"
+    ),
+    new Question(
+       "Where is the correct place to insert a JavaScript?", ["The <head> section", "The <body> section", "both A and B", "anywhere"], "both A and B",
+    ),
+    new Question(
+        "How to write an IF statement in JavaScript?", ["if i = 5 then", "if (i == 5)", 
+    "if i == 5 then", "if i = 5"], "if (i == 5)"
+    )
+];
+
+// INITIALIZE quiz
+let quiz = new Quiz(questions);
+
+// display questions
+displayQuestion();
+
+
+// Add A CountDown for the Quiz
+let time = 1;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000);
+}
+
+startCountdown();
